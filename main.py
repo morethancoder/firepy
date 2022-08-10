98474397c1c1f78bb8be65ea552bb2558778436f
Ali Taher <aalili.234.alico@gmail.com>
2022-08-10 19:01:09 +0300

    1

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ app = initialize_app(cred,{
    'databaseURL':'the URL to your database'
 })
 
-#* write data
-# we need refrence !
+# #* write data
+# # we need refrence !
 # ref = db.reference("/books/")
 # with open("./data.json","r") as f:
 #     data=json.load(f) 
 # ref.set(data)
-#* another way to write data
+# #* another way to write data
 # ref.push(data)
 # print("database has been updated ! ")
 
-#* read data
-# get ?
+# #* read data
+# # get ?
 # ref=db.reference("/books/")
 # print(ref.get())
-# get ordered data (مرتبة حسب خاصية معينة)
+# # get ordered data (مرتبة حسب خاصية معينة)
 # ordered_by_price=ref.order_by_child("price").get()
 # print("ordered by price")
 # for key,item in ordered_by_price.items():
@@ -33,14 +33,14 @@ app = initialize_app(cred,{
 #     print(f"{key} with price of : {item['price']}")
 # print(ref.order_by_child("price").equal_to(50).get())
 
-#* update data
+# #* update data
 # ref=db.reference("/books/")
 # books=ref.get()
 # for key,value in books.items():
 #     if(value["author"] == "Potato"):
 #         ref.child(key).update({"price":2})
 
-#* delete data
+# #* delete data
 # ref=db.reference("/books/")
 # ref.set({})
 # print("database has been updated ! ")
